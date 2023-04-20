96367f9e24cbd732e3e75f9aaab7fd8952215098
rzntx <112784744+rzntx@users.noreply.github.com>
2023-04-20 01:21:57 -0300

    Add files via upload

---
+++ javascriptCss.js
@@ -0,0 +1,25 @@
+function abrirLink() {
+    var abrir = document.getElementById("menuLink");
+    abrir.style.display = "block";
+    abrir.style.position = "fixed";
+    
+    
+ }
+
+ 
+
+ function fecharLink() {
+    var abrir = document.getElementById("menuLink");
+    abrir.style.display = "none";
+
+    
+    
+ }
+
+ var link = document.getElementById("listaLink");
+ link.addEventListener("mouseover", abrirLink);
+ link.addEventListener("mouseout", fecharLink);
+
+ var manter = document.getElementById("menuLink");
+ manter.addEventListener("mouseover", abrirLink);
+ manter.addEventListener("mouseout", fecharLink);
---
+++ js_terceiro.js
@@ -0,0 +1,16 @@
+function boasVindas() {
+    var nome = prompt("Digite seu nome: ");
+     if(nome == "" || nome == null || nome== false) {
+         nome = "Estudante";
+     }
+     var inNome = document.getElementById("inNome");
+ 
+     inNome.textContent = "Olá " + nome + ", seja bem-vindo(a)!";
+
+
+ }
+
+ 
+
+
+
