877505df2271934b9674921f5c88d7408b5f6667
rzntx <112784744+rzntx@users.noreply.github.com>
2023-04-24 23:15:55 -0300

    Update javascriptCss.js

--- javascriptCss.js
+++ javascriptCss.js
@@ -1,26 +1,37 @@
 function abrirLink() {
-    var abrir = document.getElementById("menuLink");
-    abrir.style.display = "block";
-    abrir.style.position = "fixed";
-    
-    
-    
+    var abrirLink = document.getElementById("menuLink");
+    abrirLink.style.display = "block";
+    abrirLink.style.position = "fixed";   
  }
 
- 
-
- function fecharLink() {
-    var abrir = document.getElementById("menuLink");
-    abrir.style.display = "none";
-
-    
-    
+function fecharLink() {
+    var abrirLink = document.getElementById("menuLink");
+    abrirLink.style.display = "none";  
  }
 
  var link = document.getElementById("listaLink");
  link.addEventListener("mouseover", abrirLink);
  link.addEventListener("mouseout", fecharLink);
 
- var manter = document.getElementById("menuLink");
- manter.addEventListener("mouseover", abrirLink);
- manter.addEventListener("mouseout", fecharLink);
+ var manterLink = document.getElementById("menuLink");
+ manterLink.addEventListener("mouseover", abrirLink);
+ manterLink.addEventListener("mouseout", fecharLink);
+
+function abrirFilmes() {
+    var abrirFilmes = document.getElementById("menuFilme");
+    abrirFilmes.style.display = "block";
+    abrirFilmes.style.position = "fixed";   
+ }
+
+function fecharFilmes() {
+    var abrirFilmes = document.getElementById("menuFilme");
+    abrirFilmes.style.display = "none";  
+ }
+
+ var filme = document.getElementById("listaFilmes");
+ filme.addEventListener("mouseover", abrirFilmes);
+ filme.addEventListener("mouseout", fecharFilmes);
+
+ var manterFilme = document.getElementById("menuFilme");
+ manterFilme.addEventListener("mouseover", abrirFilmes);
+ manterFilme.addEventListener("mouseout", fecharFilmes);
