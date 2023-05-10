c509d73dd7c4cf9952a564c2ad12ad75709611fb
rzntx <112784744+rzntx@users.noreply.github.com>
2023-05-09 23:40:43 -0300

    Update js_terceiro.js

--- js_terceiro.js
+++ js_terceiro.js
@@ -1,14 +1,16 @@
 function boasVindas() {
     var nome = prompt("Digite seu nome: ");
+    alert("Há duas atividades com prazo de entrega 16/05/2023");
+
      if(nome == "" || nome == null || nome== false) {
          nome = "Estudante";
+         
      }
      var inNome = document.getElementById("inNome");
  
      inNome.textContent = "Olá " + nome + ", seja bem-vindo(a)!";
     
-    confirm("Há duas atividades com prazo de entrega 16/05/2023");
-
+    
 
  }
 
